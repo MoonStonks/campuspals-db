95e4f871115e8bee0a2cc4e0964c9811a21b0a9f
mrbenc88 <mrbenc88@students.cs.ubc.ca>
2022-02-19 11:40:17 -0800

    configure to host on heroku

--- server.js
+++ server.js
@@ -24,6 +24,32 @@ mongoose.connect(uri, {
 
 const connection = mongoose.connection;
 
+function startKeepAlive() {
+  setInterval(function () {
+    var options = {
+      host: "campuspalsdb.herokuapp.com",
+      port: 80,
+      path: "/",
+    };
+    http
+      .get(options, function (res) {
+        res.on("data", function (chunk) {
+          try {
+            // optional logging... disable after it's working
+            console.log("HEROKU RESPONSE: " + chunk);
+          } catch (err) {
+            console.log(err.message);
+          }
+        });
+      })
+      .on("error", function (err) {
+        console.log("Error: " + err.message);
+      });
+  }, 20 * 60 * 1000); // load every 20 minutes
+}
+
+startKeepAlive();
+
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
